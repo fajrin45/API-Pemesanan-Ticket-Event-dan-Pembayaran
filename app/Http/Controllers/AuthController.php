b268a44cfae3c3b10dbf21159792d4044a21a0ba
fajrin <fajrinstar333@gmai.com>
2026-01-09 15:01:33 +0800

    add activtylog, upgrade route, upgrade authcontroller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,11 +4,19 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use App\Models\ActivityLog;
+
 
 class AuthController extends Controller
 {
     public function register(Request $request)
     {
+        $request->validate([
+            'name'     => 'required|string|max:100',
+            'email'    => 'required|email|unique:users,email',
+            'password' => 'required|min:6'
+        ]);
+
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
@@ -19,21 +27,57 @@ class AuthController extends Controller
             'success' => true,
             'message' => 'Register berhasil',
             'data' => $user
-        ]);
+        ],201);
     }
 
     public function login(Request $request)
     {
+        $request->validate([
+            'email'    => 'required|email',
+            'password' => 'required'
+        ]);
+
         if (!$token = auth('api')->attempt($request->only('email','password'))) {
             return response()->json([
                 'success' => false,
-                'message' => 'Login gagal'
+                'message' => 'Email atau password salah'
             ], 401);
         }
 
+     
+        $user = auth('api')->user();
+
+        ActivityLog::create([
+            'user_id' => $user->id,
+            'activity' => 'User login'
+        ]);
+
         return response()->json([
             'success' => true,
-            'token' => $token
+            'message' => 'Login berhasil',
+            'token' => $token,
+            'user'    => $user
+        ]);
+    }
+
+     public function profile()
+    {
+        return response()->json([
+            'success' => true,
+            'data'    => auth('api')->user()
+        ]);
+    }
+
+    /**
+     * LOGOUT USER
+     */
+    public function logout()
+    {
+        auth('api')->logout();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Logout berhasil'
         ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -9,9 +9,8 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:api')->group(function () {
-    Route::get('/profile', function () {
-        return auth()->user();
-    });
+    Route::get('/profile', [AuthController::class, 'profile']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 
     Route::post('/payments', [PaymentController::class, 'store']);
     Route::put('/payments/{id}/status', [PaymentController::class, 'updateStatus']);
