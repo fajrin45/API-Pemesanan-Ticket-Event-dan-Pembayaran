fe4cdd421c76f72389d3b80f2e01c9f34cded248
riska <riskashariseptiani@gmail.com>
2026-01-11 10:59:33 +0800

    add ticket table

---
+++ database/migrations/2026_01_10_154757_create_tikets_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tikets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('event_id')->constrained('events')->onDelete('cascade');
+            $table->string('jenis_tiket'); // VIP, Regular, Early Bird, dll
+            $table->decimal('harga', 12, 2);
+            $table->integer('kuota');
+            $table->integer('terjual')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tikets');
+    }
+};
